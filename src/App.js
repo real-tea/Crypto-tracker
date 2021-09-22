ffef5b3a0e74d5a53a38ea7bd01e867b8a395501
Akash <akash.0711a@gmail.com>
2021-09-22 18:37:00 +0530

    coin components and axios json fetching from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import logo from './logo.svg';
+import React,{ useState,useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
 function App() {
+
+  const [coins,setCoins] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then((res) => {
+      setCoins(res.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  },[])
+
   return (
     <div className="App">
+      <div className = "coin">
+        <h1>Search for a crypto currency</h1>
+        <form>
+          <input type = "text"
+           placeholder = "search"
+           className = "coin-text"
+           />
+        </form>
+      </div>
       
     </div>
   );
