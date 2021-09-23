70f8f671952cd0de3b41c516b5c511dd072bad77
Akash <akash.0711a@gmail.com>
2021-09-23 19:34:19 +0530

    new commits

--- src/components/coins.jsx
+++ src/components/coins.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Coins = (props) => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <img src = { props.image } alt = "web3 crypto"/>
+                    <h1>{ props.name }</h1>
+                    <p>{ props.symbol }</p>
+
+                    <div>
+                        <p>${ props.price }</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
